ef50cd58b651efe2de6f2b710a8ef2278ab595e5
Kazuya Ueoka <k.ueoka@timers-inc.com>
2015-11-09 13:58:39 +0900

    Initial Commit

---
+++ Demo1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo1
+//
+//  Created by Kazuya Ueoka on 2015/11/09.
+//  Copyright © 2015年 TImers Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Demo1/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Demo1
+//
+//  Created by Kazuya Ueoka on 2015/11/09.
+//  Copyright © 2015年 TImers Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
